e742a1912e571ebdcab6f8e7f7429a0111eec641
Zeta <hey@zzev.me>
2024-07-23 19:37:52 -0600

    Add default site

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://example.com',
   integrations: [react(), tailwind(), sitemap(), robotsTxt()]
 });
